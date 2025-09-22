6136f6d446f095d9fd5738048606306c8ab03e39
David Scandurra <david.scandurra@check24.de>
2025-09-22 04:16:16 +0200

    Cleanup template
    Signed-off-by: David Scandurra <31861387+SplotyCode@users.noreply.github.com>

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,12 +1,8 @@
 dependencyResolutionManagement {
-
-    // Use Maven Central and the Gradle Plugin Portal for resolving dependencies in the shared build logic (`buildSrc`) project.
     @Suppress("UnstableApiUsage")
     repositories {
         mavenCentral()
     }
-
-    // Reuse the version catalog from the main build.
     versionCatalogs {
         create("libs") {
             from(files("../gradle/libs.versions.toml"))
