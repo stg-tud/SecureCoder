5d08f9d20f4f26d240767f8804ccadd56b015e8a
David Scandurra <31861387+SplotyCode@users.noreply.github.com>
2025-10-05 02:56:08 +0200

    Report uncaught engine exception in IntelliJ-plugin

--- app/intellij-plugin/src/main/java/de/tuda/stg/securecoder/plugin/engine/EngineRunnerService.kt
+++ app/intellij-plugin/src/main/java/de/tuda/stg/securecoder/plugin/engine/EngineRunnerService.kt
@@ -1,9 +1,11 @@
 package de.tuda.stg.securecoder.plugin.engine
 
 import com.intellij.openapi.components.Service
+import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
 import com.intellij.platform.ide.progress.withBackgroundProgress
 import de.tuda.stg.securecoder.engine.llm.OllamaClient
+import de.tuda.stg.securecoder.engine.stream.EventIcon
 import de.tuda.stg.securecoder.engine.stream.StreamEvent
 import de.tuda.stg.securecoder.engine.workflow.WorkflowEngine
 import de.tuda.stg.securecoder.enricher.EnricherClient
@@ -32,8 +34,19 @@ class EngineRunnerService(
         cs.launch(Dispatchers.IO) {
             withBackgroundProgress(project, "Running engine…", cancellable = false) {
                 val fileSystem = IntelliJProjectFileSystem(project)
-                engine.start(text, fileSystem, onEvent)
-                onComplete()
+                try {
+                    engine.start(text, fileSystem, onEvent)
+                } catch (exception: Exception) {
+                    thisLogger().error("Uncaught exception within the engine", exception)
+                    onEvent(StreamEvent.Message(
+                        "Internal engine error",
+                        "Uncaught exception within the engine."
+                                + " This should not happen and is always a bug: ${exception.message} (${exception.javaClass.simpleName})",
+                        EventIcon.Error
+                    ))
+                } finally {
+                    onComplete()
+                }
             }
         }
     }
