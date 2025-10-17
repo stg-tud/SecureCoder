3863d2fd1a0754cb01ad3c910cc6526b68aef869
David Scandurra <31861387+SplotyCode@users.noreply.github.com>
2025-10-17 11:39:14 +0200

    Add api for guardian

---
+++ guardian/api/src/main/java/de/tuda/stg/securecoder/guardian/Models.kt
@@ -0,0 +1,44 @@
+package de.tuda.stg.securecoder.guardian
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class File(
+    val name: String,
+    val content: String,
+)
+
+@Serializable
+data class AnalyzeRequest(val files: List<File>) /* TODO do we need ALL files? */
+
+@Serializable
+data class AnalyzeResponse(
+    val hardReject: Boolean,
+    val violations: List<Violation>,
+)
+
+@Serializable
+data class Violation(
+    val rule: RuleRef,
+    val message: String,
+    val location: Location,
+    val confidence: String? = null,
+)
+
+@Serializable
+data class RuleRef(
+    val id: String,
+    val name: String? = null,
+    val description: String? = null,
+    val cwe: String? = null,
+    val owasp: String? = null
+)
+
+@Serializable
+data class Location(
+    val file: String,
+    val startLine: Int? = null,
+    val endLine: Int? = null,
+    val startColumn: Int? = null,
+    val endColumn: Int? = null
+)
