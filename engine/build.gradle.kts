60a9306caaf291d5e33ac6320b338596feead17e
David Scandurra <31861387+SplotyCode@users.noreply.github.com>
2025-10-21 11:14:19 +0200

    Add Guardian API dependency to engine

--- engine/build.gradle.kts
+++ engine/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 
 dependencies {
     api(project(":enricher:api"))
+    implementation(project(":guardian:api"))
     implementation(libs.ktor.client.java)
     implementation(libs.ktor.client.content.negotiation)
     implementation(libs.ktor.serialization.json)
