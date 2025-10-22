689b2afaa9a0199e7a3dcc4d3b004b586ba2e3b0
David Scandurra <31861387+SplotyCode@users.noreply.github.com>
2025-10-22 18:04:02 +0200

    Expose guardian api as gradle api dependency

--- engine/build.gradle.kts
+++ engine/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 dependencies {
     api(project(":enricher:api"))
-    implementation(project(":guardian:api"))
+    api(project(":guardian:api"))
     implementation(libs.ktor.client.java)
     implementation(libs.ktor.client.content.negotiation)
     implementation(libs.ktor.serialization.json)
